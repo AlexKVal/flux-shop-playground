bbd57b367fba50ae5a7cc7fde43c530cac28df7a
AlexKVal <alexkval@gmail.com>
2015-03-13 23:09:26 +0200

    add envify. but it had not effect on the resulting size

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ module.exports = {
   output: { filename: 'dist/js/main.js' },
   module: {
     loaders: [
-      { test: /\.jsx$/, loader: 'jsx-loader' }
+      { test: /\.jsx$/, loader: 'jsx-loader' },
+      { test: /\.js$/, loader: 'envify-loader' }
     ]
   },
   resolve: {
