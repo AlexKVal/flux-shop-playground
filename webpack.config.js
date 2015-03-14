2925e2f1ac8b35e3698a3d9cf345ac839b14efcc
AlexKVal <alexkval@gmail.com>
2015-03-14 17:37:01 +0200

    remove superfluous code

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,7 @@
 module.exports = {
-  // watch: true,
-  entry: './src/js/main.jsx',
-  output: {
-    path: "dist",
-    // publicPath: "/",
-    filename: 'js/main.js'
-  },
   module: {
     loaders: [
-      { test: /\.jsx$/, loader: 'jsx-loader' } //,
-      // { test: /\.js$/, loader: 'envify-loader' }
+      { test: /\.jsx$/, loader: 'jsx-loader' }
     ]
   },
   resolve: {
